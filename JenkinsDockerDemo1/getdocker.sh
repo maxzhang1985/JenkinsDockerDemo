4b4a73c99aa4f23babff2d6cabba23de281c0128
max zhang <zl.hxd@hotmail.com>
2017-06-09 14:39:53 +0800

    99

--- JenkinsDockerDemo1/getdocker.sh
+++ JenkinsDockerDemo1/getdocker.sh
@@ -1,7 +1,9 @@
-﻿docker stop myweb
+﻿#!/bin/bash
+
+docker stop myweb
 
 docker rm myweb
 
 docker build --rm -t dotnetweb .
 
-docker run --name myweb  -d dotnetweb
+docker run --name myweb -p 5000:80  -d dotnetweb
